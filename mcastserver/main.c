3775cdc1fe7fbb8b9a9d01897bb6edad0b1e9534
Thierry DELHAISE <thierry.delhaise@gmail.com>
2024-03-04 12:05:54 +0100

    Initial Commit

---
+++ mcastserver/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mcastserver
+//
+//  Created by Thierry DELHAISE on 04/03/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
