8c574a6e7232aab2a2c91f2126f977315d1828f9
Thierry DELHAISE <thierry.delhaise@gmail.com>
2024-03-13 00:57:03 +0100

    More compatibility with Linux system

--- mcastserver/NetworkInterface.c
+++ mcastserver/NetworkInterface.c
@@ -16,7 +16,7 @@ const char* NetworkInterfaceGetFamillyName(struct sockaddr* socketAddress) {
         return "ERROR";
     }
 
-#ifdef __APPLE__
+
     switch (socketAddress->sa_family) {
         case AF_UNSPEC:
             return "AF_UNSPEC";
@@ -24,6 +24,23 @@ const char* NetworkInterfaceGetFamillyName(struct sockaddr* socketAddress) {
             return "AF_UNIX";
         case AF_INET:
             return "AF_INET";
+        case AF_SNA:
+            return "AF_SNA";
+        case AF_DECnet:
+            return "AF_DECnet";
+        case AF_APPLETALK:
+            return "AF_APPLETALK";
+        case AF_ROUTE:
+            return "AF_ROUTE";
+        case AF_IPX:
+            return "AF_IPX";
+        case AF_LINK:
+            return "AF_LINK";
+        case AF_INET6:
+            return "AF_INET6";
+        case AF_ISDN:
+            return "AF_ISDN";
+#ifdef __APPLE__
         case AF_IMPLINK:
             return "AF_IMPLINK";
         case AF_PUP:
@@ -40,22 +57,12 @@ const char* NetworkInterfaceGetFamillyName(struct sockaddr* socketAddress) {
             return "AF_DATAKIT";
         case AF_CCITT:
             return "AF_CCITT";
-        case AF_SNA:
-            return "AF_SNA";
-        case AF_DECnet:
-            return "AF_DECnet";
         case AF_DLI:
             return "AF_DLI";
         case AF_LAT:
             return "AF_LAT";
         case AF_HYLINK:
             return "AF_HYLINK";
-        case AF_APPLETALK:
-            return "AF_APPLETALK";
-        case AF_ROUTE:
-            return "AF_ROUTE";
-        case AF_LINK:
-            return "AF_LINK";
         case pseudo_AF_XTP:
             return "pseudo_AF_XTP";
         case AF_COIP:
@@ -64,20 +71,14 @@ const char* NetworkInterfaceGetFamillyName(struct sockaddr* socketAddress) {
             return "AF_CNT";
         case pseudo_AF_RTIP:
             return "pseudo_AF_RTIP";
-        case AF_IPX:
-            return "AF_IPX";
         case AF_SIP:
             return "AF_SIP";
         case pseudo_AF_PIP:
             return "pseudo_AF_PIP";
         case AF_NDRV:
             return "AF_NDRV";
-        case AF_ISDN:
-            return "AF_ISDN";
         case pseudo_AF_KEY:
             return "pseudo_AF_KEY";
-        case AF_INET6:
-            return "AF_INET6";
         case AF_NATM:
             return "AF_NATM";
         case AF_SYSTEM:
@@ -94,36 +95,23 @@ const char* NetworkInterfaceGetFamillyName(struct sockaddr* socketAddress) {
             return "AF_IEEE80211";
         case AF_UTUN:
             return "AF_UTUN";
-        case AF_VSOCK:
-            return "AF_VSOCK";
-        case AF_MAX:
-            return "AF_MAX";
-        default:
-            return "UNKNOWN";
-    }
 #endif
 #ifdef __linux__
-    switch (socketAddress->sa_family) {
-        case AF_UNSPEC:
-            return "AF_UNSPEC";
-        case AF_UNIX:
-            return "AF_UNIX";
-        case AF_INET:
-            return "AF_INET";
-        case AF_SNA:
-            return "AF_SNA";
-        case AF_DECnet:
-            return "AF_DECnet";
-        case AF_APPLETALK:
-            return "AF_APPLETALK";
-        case AF_ROUTE:
-            return "AF_ROUTE";
-        case AF_IPX:
-            return "AF_IPX";
-        case AF_ISDN:
-            return "AF_ISDN";
-        case AF_INET6:
-            return "AF_INET6";
+        case IFF_ALLMULTI:
+            return "IFF_ALLMULTI";
+        case IFF_MASTER:
+            return "IFF_MASTER";
+        case IFF_SLAVE:
+            return "IFF_SLAVE";
+        case IFF_MULTICAST:
+            return "IFF_MULTICAST";
+        case IFF_PORTSEL:
+            return "IFF_PORTSEL";
+        case IFF_AUTOMEDIA:
+            return "IFF_AUTOMEDIA";
+        case IFF_DYNAMIC:
+            return "IFF_DYNAMIC";
+#endif
         case AF_VSOCK:
             return "AF_VSOCK";
         case AF_MAX:
@@ -131,7 +119,6 @@ const char* NetworkInterfaceGetFamillyName(struct sockaddr* socketAddress) {
         default:
             return "UNKNOWN";
     }
-#endif
 }
 
 #define MIN_DISPLAY_FLAGS_LENGTH    15*16
