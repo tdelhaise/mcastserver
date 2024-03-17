31fba6df5963677c39fea36147b3f5ab4a216f43
Thierry DELHAISE <thierry.delhaise@gmail.com>
2024-03-17 05:40:39 +0100

    Correct mcastclient for sending a correct packet

--- mcastclient/main.c
+++ mcastclient/main.c
@@ -26,9 +26,11 @@ int main(int argc, char *argv[])
     int              socketFileDescriptor = -1;
     int              status = 0;
     char             buf[BUF_SIZE];
-    size_t           len;
+    size_t           len = 0;
+    ssize_t          nread = 0;
+    ssize_t          nsent = 0;
     struct addrinfo  hints;
-    struct addrinfo  *result, *rp;
+    struct addrinfo  *resultAddresses, *resultAddress;
     char portNumber[MAX_PORT_NUMBER];
     uint16_t portAsInteger = -1;
     char hostname[MAX_HOSTNAME];
@@ -72,10 +74,10 @@ int main(int argc, char *argv[])
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-    hints.ai_flags = 0;
+    hints.ai_flags = AI_ADDRCONFIG ;
     hints.ai_protocol = 0;          /* Any protocol */
 
-    status = getaddrinfo(hostname, portNumber, &hints, &result);
+    status = getaddrinfo(hostname, portNumber, &hints, &resultAddresses);
     if (status != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
@@ -86,98 +88,57 @@ int main(int argc, char *argv[])
        If socket(2) (or connect(2)) fails, we (close the socket
        and) try the next address. */
 
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        fprintf(stdout, "socket family [%d] type [%d] proto [%d]\n",rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        socketFileDescriptor = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (socketFileDescriptor == -1)
+    for (resultAddress = resultAddresses; resultAddress != NULL; resultAddress = resultAddress->ai_next) {
+        fprintf(stdout, "socket family [%d] type [%d] proto [%d]",resultAddress->ai_family, resultAddress->ai_socktype, resultAddress->ai_protocol);
+        socketFileDescriptor = socket(resultAddress->ai_family, resultAddress->ai_socktype, resultAddress->ai_protocol);
+        if (socketFileDescriptor == -1) {
+            fprintf(stdout, " -> Abandonned.\n");
             continue;
+        }
         
-        // set up destination address
-        //
-        struct sockaddr_in addr;
-        memset(&addr, 0, sizeof(addr));
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = htonl(INADDR_ANY); // differs from sender
-        addr.sin_port = htons(portAsInteger);
-        //
-        // bind to receive address
-        //
-        if (bind(socketFileDescriptor, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
-            perror("bind");
-            close(socketFileDescriptor);
-            continue;
-        }
-        //
-        // use setsockopt() to request that the kernel join a multicast group
-        //
-        struct ip_mreq mreq;
-        mreq.imr_multiaddr.s_addr = inet_addr(hostname);
-        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-        if (setsockopt(socketFileDescriptor, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*) &mreq, sizeof(mreq)) < 0 ) {
-            perror("setsockopt");
-            close(socketFileDescriptor);
-            continue;
-        }
-        /* Success */
-        break;
+        fprintf(stdout, " -> Selected.\n");
+        
+        if ( connect(socketFileDescriptor, resultAddress->ai_addr, resultAddress->ai_addrlen) != -1 )
+            break;
+        
+        close(socketFileDescriptor);
+        
     }
 
-    freeaddrinfo(result);           /* No longer needed */
+    freeaddrinfo(resultAddresses);           /* No longer needed */
 
-    if (rp == NULL) {               /* No address succeeded */
+    if (resultAddress == NULL) {               /* No address succeeded */
         fprintf(stderr, "Could not bind\n");
         exit(EXIT_FAILURE);
     }
 
-    /* Send remaining command-line arguments as separate
-       datagrams, and read responses from server. */
-    for (size_t j = 3; j < argc; j++) {
-        len = strlen(message) + 1;
-                /* +1 for terminating null byte */
+    len = strlen(message) + 1;
+            /* +1 for terminating null byte */
 
-        if (len > BUF_SIZE) {
-            fprintf(stderr, "Ignoring long message in argument %zu\n", j);
-            continue;
-        }
-        
-        // set up destination address
-        //
-        struct sockaddr_storage peerAddress;
-        memset(&peerAddress, 0, sizeof(peerAddress));
-        socklen_t peerAddressLength = sizeof(peerAddress);
-        
-        ssize_t sentBytes = sendto(socketFileDescriptor, message, len, 0, (struct sockaddr *) &peerAddress, peerAddressLength);
-        if (sentBytes != len) {
-            perror("sendto");
-            fprintf(stderr, "Error sending message\n");
-            exit(EXIT_FAILURE);
-        } else {
-            fprintf(stdout, "Sent %ld byte(s) \n", sentBytes);
-        }
-
-        /*
-        if (write(socketFileDescriptor, message, len) != len) {
-            fprintf(stderr, "partial/failed write\n");
-            exit(EXIT_FAILURE);
-        }
-        
-        nread = read(socketFileDescriptor, buf, BUF_SIZE);
-        if (nread == -1) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
-        */
-        
-        ssize_t nbytes = recvfrom( socketFileDescriptor, buf, BUF_SIZE, 0, (struct sockaddr *) &peerAddress, &peerAddressLength);
-        if (nbytes < 0) {
-            perror("recvfrom");
-            exit(EXIT_FAILURE);
-        }
-        buf[nbytes] = '\0';
-
-
-        printf("Received %zd bytes: %s\n", nbytes, buf);
+    if (len > BUF_SIZE) {
+        fprintf(stderr, "Ignoring long message in argument [%s]\n", message);
+        close(socketFileDescriptor);
+        exit(EXIT_FAILURE);
     }
+    
+    nsent = write(socketFileDescriptor, message, len);
+    if (nsent != len) {
+        fprintf(stderr, "partial/failed write\n");
+        close(socketFileDescriptor);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Sent %zd bytes: %s\n", nsent, message);
+    
+    nread = read(socketFileDescriptor, buf, BUF_SIZE);
+    if (nread == -1) {
+        perror("read");
+        close(socketFileDescriptor);
+        exit(EXIT_FAILURE);
+    }
+    
+    buf[nread] = '\0';
+    printf("Received %zd bytes: %s\n", nread, buf);
     
     close(socketFileDescriptor);
 
